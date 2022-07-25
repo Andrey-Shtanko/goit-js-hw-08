36cb0469e2784ac3ea8dc46281900ec561c28780
Andrey-Shtanko <97941755+Andrey-Shtanko@users.noreply.github.com>
2022-07-25 15:37:03 +0300

    task-03-without-throttle

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,9 +1,12 @@
 import throttle from "lodash.throttle";
 
 const form = document.querySelector(`.feedback-form`)
+const emailInput = document.querySelector(`input`)
+const textareaInput = document.querySelector(`textarea`)
 
 form.addEventListener(`input`, onFormInput)
 form.addEventListener(`submit`, onFormSubmit)
+currentInputValues()
 
 function onFormInput (event) {
     const { elements: { email, message } } = event.currentTarget
@@ -15,7 +18,24 @@ function onFormInput (event) {
     localStorage.setItem("feedback-form-state", JSON.stringify(objValues))
 }
 
+
+
 function onFormSubmit(event) {
     event.preventDefault()
     event.currentTarget.reset()
+    const savesData = JSON.parse(localStorage.getItem("feedback-form-state"))
+    console.log(savesData);
+    localStorage.removeItem("feedback-form-state")
+}
+
+
+function currentInputValues() {
+    const savesData = JSON.parse(localStorage.getItem("feedback-form-state"))
+
+    if (savesData) {
+        const { email, message } = savesData
+        emailInput.value = email
+        textareaInput.value = message
+    }
+    
 }
