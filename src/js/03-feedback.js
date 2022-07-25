bc40189ef4aece3e8f758f0130351ebc1c1ea4ce
Andrey-Shtanko <97941755+Andrey-Shtanko@users.noreply.github.com>
2022-07-25 14:59:22 +0300

    tosk-03-in-proccess

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,21 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector(`.feedback-form`)
+
+form.addEventListener(`input`, onFormInput)
+form.addEventListener(`submit`, onFormSubmit)
+
+function onFormInput (event) {
+    const { elements: { email, message } } = event.currentTarget
+    
+    const objValues = {
+        email: email.value,
+        message: message.value,
+    }
+    localStorage.setItem("feedback-form-state", JSON.stringify(objValues))
+}
+
+function onFormSubmit(event) {
+    event.preventDefault()
+    event.currentTarget.reset()
+}
