3028ab957f7853c1940d3c4c2ca9a6fb09e9d4c8
Andrey-Shtanko <97941755+Andrey-Shtanko@users.noreply.github.com>
2022-07-25 10:19:01 +0300

    task-01-done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,15 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector(`.gallery`);
+const galleryItemsMarkUp = galleryItems.map(({ preview, original, description }) =>
+    `<li><a class = "gallery__item" href="${original}"><img class = "gallery__image" src = "${preview}" alt = "${description}" title = "${description}" ></a></li>`
+).join(``)
+gallery.innerHTML = galleryItemsMarkUp;
+
+var lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
