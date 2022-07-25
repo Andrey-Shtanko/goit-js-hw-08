c033fefb9d105d3c4d9070c3e6d439328bfca888
Andrey-Shtanko <97941755+Andrey-Shtanko@users.noreply.github.com>
2022-07-25 13:03:18 +0300

    task-02-without-throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
 
+
+
+const iframe = document.querySelector('iframe');
+    
+const player = new Player(iframe);
+
+
+const time = function (currentTime) {
+    let savedTime = 0;
+    let { seconds } = currentTime
+     savedTime =  localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds))
+        
+};
+
+player.on('timeupdate', time);
+
+const savedCurrentTime = JSON.parse(localStorage.getItem("videoplayer-current-time"))
+
+player.setCurrentTime(JSON.parse(savedCurrentTime));
